a21e00142afc886e4aa581c5817e1e450098c065
jainishpathak <pathakjainish@gmail.com>
2023-10-11 13:57:12 +0530

    res to response

--- Backend/index.js
+++ Backend/index.js
@@ -1,22 +1,24 @@
-import express from "express";
-import { PORT } from "./config.js";
-import { Book } from "./models/bookmodel.js";
-import connectToMongo from "./database/db.js"
-const app = express()
-connectToMongo()
-app.use(express.json());
+ import express from "express";
+ import { PORT, mongoDBURL } from "./config.js";
+ import mongoose  from "mongoose";
+ import { Book } from "./models/bookmodel.js";
+ import connectToMongo from "./database/db.js";
 
-app.get('/', (request, response) => {
-   console.log(request);
-   return response.status(234).send('Hello')
-});
+ const app = express()
 
-app.post('/books', async (request, response) => {
-   try {
-      if (
-         !res.body.title ||
-         !res.body.author ||
-         !res.body.publisYear
+ app.use(express.json());
+
+ app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('Hello')
+ });
+
+ app.post('/books', async (request,response) => {
+   try{
+      if(
+         !response.body.title ||
+         !response.body.author ||
+         !response.body.publisYear
       ) {
          return response.status(400).send({
             message: 'Send all required fileds: title, author, publishYear'
@@ -32,12 +34,20 @@ app.post('/books', async (request, response) => {
 
       return response.status(201).send(book);
 
-   } catch (error) {
+   } catch(error){
       console.log(error.message);
-      response.status(500).send({ message: error.message });
+      response.status(500).send({ message: error.message});
    }
-});
+ });
 
-app.listen(PORT, () => {
-   console.log(`App listening to port : ${PORT}`);
-});
+//  mongoose
+//    .connect(mongoDBURL)
+//    .then(() => {
+//       console.log('App connected  to database');
+//       app.listen(PORT, () => {
+//          console.log(`App listening to port : ${PORT}`);
+//       });
+//    })
+//    .catch((error) => {
+//       console.log(error);
+//    });
