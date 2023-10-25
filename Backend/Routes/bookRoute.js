4069c9486ba87bcd85dcfcbe94ab1e37727c6280
jainishpathak <pathakjainish@gmail.com>
2023-10-25 21:13:21 +0530

    Backend portion competed

---
+++ Backend/Routes/bookRoute.js
@@ -0,0 +1,105 @@
+import express from "express";
+import { Book } from "../models/bookmodel.js";
+const rt = express.Router();
+
+rt.post('/', async (request, response) => {
+    try {
+       if (
+          !request.body.title ||
+          !request.body.author ||
+          !request.body.publishYear
+       ) {
+          return response.status(400).send({
+             message: 'Send all required fileds: title, author, publishYear'
+          });
+       }
+       const newBook = {
+          title: request.body.title,
+          author: request.body.author,
+          publishYear: request.body.publishYear,
+       };
+ 
+       const book = await Book.create(newBook);
+ 
+       return response.status(201).send(book);
+ 
+    } catch (error) {
+       console.log(error.message);
+       response.status(500).send({ message: error.message });
+    }
+ });
+ 
+ rt.get('/', async (req, res) => {
+    try{
+       const books = await Book.find({});
+       return res.status(200).json({
+          count: books.length,
+          data: books,
+       });
+    } catch(error){
+       console.log(error.message);
+       res.status(500).send({message: error.message})
+    }
+ })
+ 
+ rt.get('/:id', async (req, res) => {
+    try{
+ 
+       const{ id } = req.params;
+ 
+       const book = await Book.findById(id);
+ 
+       return res.status(200).json({book});
+    } catch(error){
+       console.log(error.message);
+       res.status(500).send({message: error.message})
+    }
+ })
+ 
+ rt.put('/:id', async (req,res) => {
+    try{
+       if (
+          !req.body.title ||
+          !req.body.author ||
+          !req.body.publishYear
+       ) {
+          return response.status(400).send({
+             message: 'Send all required fileds: title, author, publishYear'
+          });
+       }
+ 
+       const { id } = req.params;
+ 
+       const result = await Book.findByIdAndUpdate(id, req.body);
+ 
+       if(!result){
+          return res.status(404).json({message: 'Book not found.'});
+       }
+       return res.status(404).json({message: 'Book updated succesfully.'});
+ 
+    } catch (error){
+       console.log(error.message);
+       res.status(500).send({message: error.message})
+    }
+ });
+ 
+ rt.delete('/:id', async (req,res) => {
+    try{
+       
+ 
+       const { id } = req.params;
+ 
+       const result = await Book.findByIdAndDelete(id);
+ 
+       if(!result){
+          return res.status(404).json({message: 'Book not found.'});
+       }
+       return res.status(404).json({message: 'Book deleted succesfully.'});
+ 
+    } catch (error){
+       console.log(error.message);
+       res.status(500).send({message: error.message})
+    }
+ });
+
+ export default rt;
