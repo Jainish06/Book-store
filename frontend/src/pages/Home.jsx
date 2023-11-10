0eebf51eab16bd3b95e7c38a7e5fffb88a02765d
jainishpathak <pathakjainish@gmail.com>
2023-11-10 13:33:10 +0530

    home page start

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import React,{useEffect, useState} from 'react';
-import axios from 'axios';
 import Spinner from '../components/Spinner';
 import { Link } from 'react-router-dom';
 import { AiOutlineEdit } from 'react-icons/ai';
